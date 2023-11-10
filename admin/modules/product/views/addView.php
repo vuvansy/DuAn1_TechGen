a7ea783f25fbbfe86e5898bf36a9ced8a9eb2d79
NnKha <nhatkha2018@gmail.com>
2023-11-10 19:08:50 +0700

    features/admin-product_nk

--- admin/modules/product/views/addView.php
+++ admin/modules/product/views/addView.php
@@ -1,7 +1,20 @@
 <?php
 get_header();
 ?>
+<main class="layout-fullscreen">
+    <section class="menu">
+    </section>
+    <section class="product-container">
+        <div class="product-heading">
+            Danh sách hàng hóa
+        </div>
 
+        <div class="product-bar">
+            <a href="#!" class="product-btn-add">Thêm mới</a>
+            <a href="#!" class="product-btn-list">Danh sách</a>
+        </div>
+    </section>
+</main>
 
 <?php
 get_footer();
---
+++ admin/modules/product/views/listView.php
@@ -0,0 +1,8 @@
+<?php
+get_header();
+?>
+<h1>kha đẹp trai</h1>
+
+<?php
+get_footer();
+?>
