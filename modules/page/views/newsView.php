3a3cdd5644cca4af3593ac4617c8b87de682084c
mtienha <tiennmpd08296@fpt.edu.vn>
2023-11-10 19:16:09 +0700

    features/page_t

---
+++ modules/page/views/newsView.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tin tức</title>
+</head>
+<body>
+
+<?php
+get_header();
+//show_array($list_users);
+?>
+
+<main>
+    <div class="news-item">
+        <div class="news-title">
+            <h2>TECHGEN liên kết cùng các đơn vị tài chính ưu đãi trả góp iPhone 15 series</h2>
+        </div>
+        <div class="news-content">
+            <p>Theo thông tin được công bố chính thức tại sự kiện Apple 2023 diễn ra vào rạng sáng ngày 13/09, dự đoán iPhone 15 sẽ chính thức bán ra tại Việt Nam vào khoảng thời gian cuối tháng 9. Nhằm mang đến cho các khách hàng cơ hội sở hữu iPhone 15 series sớm nhất với mức ưu đãi tốt nhất, TECHGEN đã hợp tác cùng các đơn vị tài chính triển khai chương trình ưu đãi trả góp dành cho iPhone 15 series.</p>
+            <img class="news-image" src="public/images/postpage/page5" alt=" ảnh sản phẩm IPhone 15 promax">
+            <p>Để hỗ trợ khách hàng sở hữu được siêu phẩm iPhone 15 Series, TECHGEN hân hạnh mang đến giải pháp thanh toán trả góp với mức lãi suất ưu đãi từ 0% qua các công ty tài chính Home Credit, Mirae Asset… Cụ thể, Khách hàng sẽ được hỗ trợ các gói trả góp sau:</p>
+            <div class="news-details">
+            <p>- Gói lãi suất ưu đãi: Trả góp 0% lãi suất hoặc 0.5% kèm khuyến mãi hấp dẫn
+                + Khách hàng phải trả trước số tiền từ 30% giá máy, kỳ hạn thanh toán từ 6-8 tháng.
+                + Chương trình chỉ áp dụng cho một số mẫu máy iPhone 15 series.</p>
+            <p>- Gói lãi suất độc quyền tại TECHGEN: Ưu đãi trả góp chỉ 1.54%/tháng giảm ngay 1.5M từ giá bán, với số tiền trả góp hàng chỉ từ 1.8M qua nhà tài chính Mirae Asset.
+                + Chương trình áp dụng với tất cả mẫu máy iPhone 15 series
+                + Khách hàng có thể chọn thanh toán với kỳ hạn lên đến 24 tháng.</p>
+                <p>Thời gian áp dụng chương trình: từ 22/9/2023 cho đến khi có thông báo mới.</p>
+                <p class="news-time">Sản phẩm mua tại TECHGEN là hàng chính hãng, khách hàng có thể chọn mua trực tiếp tại cửa hàng, mua online hoặc gọi hotline 1900 1000 để được tư vấn, mua hàng nhanh.</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php
+get_footer();
+?> 
