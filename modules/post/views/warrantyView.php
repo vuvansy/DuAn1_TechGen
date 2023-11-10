f79ca6f3d743673dc23c1488ea550604a43b0cc6
mtienha <tiennmpd08296@fpt.edu.vn>
2023-11-10 17:32:28 +0700

    features/page_t

--- modules/post/views/warrantyView.php
+++ modules/post/views/warrantyView.php
@@ -4,8 +4,15 @@ get_header();
 ?>
 
 <main>
-    <h1>tien</h1>
-    <p>?mod=post&action=warranty</p>
+    <div>
+        <div>
+            <h1>Chính sách bảo hành</h1>
+        </div>
+        <div>
+            <p>TECHGEN chân thành cảm ơn Quý khách hàng, dưới đây là chi tiết chính sách bảo hành của TECHGEN áp dụng với tất cả các sản phẩm, TECHGEN luôn nỗ lực để có được chất lượng dịch vụ tốt nhất dành cho Quý khách hàng.</p>
+        </div>
+        
+    </div>
 </main>
 
 <?php
