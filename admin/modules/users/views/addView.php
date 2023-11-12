4fda32f2fec6b514775a9d0eec06d29165054b8c
NnKha <nhatkha2018@gmail.com>
2023-11-12 22:24:26 +0700

    features/admin-user_nk

---
+++ admin/modules/users/views/addView.php
@@ -0,0 +1,16 @@
+<?php
+get_header();
+?>
+<main>
+    <div class="main-wrapper">
+        <?php
+        get_sidebar();
+        ?>
+        <div class="sidebar-right">
+        </div>
+    </div>
+</main>
+
+<?php
+get_footer();
+?>
