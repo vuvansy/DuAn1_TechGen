e1b348aca8babbf8e4ed16619bbe031f4572fadf
vuvansy <vuvansy3001@gmail.com>
2023-11-12 14:20:26 +0700

    features/user_s Code page newPassword and fix Bug...

--- modules/users/views/newPassView.php
+++ modules/users/views/newPassView.php
@@ -3,56 +3,36 @@ get_header();
 ?>
 
 <main>
-    <!-- Update Account -->
-    <section class="register">
+    <!-- Reset New password Account -->
+    <section class="newPassword">
         <div class="container">
             <div class="form__account">
                 <div class="account__inner">
-                    <h1 class="heading-title">CẬP NHẬT THÔNG TIN</h1>
+                    <h1 class="heading-title">ĐỔI MẬT KHẨU</h1>
                     <form action="" id="form-login" method="POST">
-                        <div class="row-form__group">
-                            <div class="form__group">
-                                <label for="fullname">Họ tên</label>
-                                <input type="text" name="fullname" id="fullname" value="<?php echo set_value('fullname') ?>" placeholder="Fullname..." />
-                                <?php echo form_error('fullname'); ?>
-                            </div>
 
-                            <div class="form__group">
-                                <label for="username">Tên đăng nhập</label>
-                                <input type="text" name="username" id="username" value="<?php echo set_value('username') ?>" readonly="readonly" placeholder="Username..." />
-                                <?php echo form_error('username'); ?>
-                            </div>
-                        </div>
-
-                        <div class="row-form__group">
-                            <div class="form__group">
-                                <label for="email">Email</label>
-                                <input type="text" name="email" id="email" value="" placeholder="Email..." />
-                                <?php echo form_error('email'); ?>
-                            </div>
-
-                            <div class="form__group">
-                                <label for="phone">Số Điện thoại</label>
-                                <input type="text" name="phone" id="phone" value="" placeholder="Số điện thoại..." />
-                                <?php echo form_error('phone'); ?>
-                            </div>
+                        <div class="form__group">
+                            <label for="old-pass">Mật khẩu cũ</label>
+                            <input type="password" name="pass-old" id="pass-old" placeholder="Mật khẩu cũ">
+                            <?php echo form_error('pass-old'); ?>
                         </div>
 
                         <div class="form__group">
-                            <label for="address">Địa chỉ</label>
-                            <input type="text" name="address" id="address" value="" placeholder="Địa chỉ..." />
-                            <?php echo form_error('address'); ?>
+                            <label for="new-pass">Mật khẩu mới</label>
+                            <input type="password" name="pass-new" id="pass-new" placeholder="Mật khẩu mới">
+                            <?php echo form_error('pass-new'); ?>
                         </div>
 
                         <div class="form__group">
-                            <label for="image" class="image__user">Hình</label>
-                            <input type="file" name="image" id="image" value="" placeholder="Địa chỉ..." />
-                            <?php echo form_error('image'); ?>
+                            <label for="confirm-pass">Xác nhận mật khẩu</label>
+                            <input type="password" name="confirm-pass" id="confirm-pass" placeholder="Mật khẩu mới">
+                            <?php echo form_error('confirm-pass'); ?>
                         </div>
 
-                        <input type="submit" name="btn-update" class="btn user__cta" id="btn-login" value="CẬP NHẬT" />
+                        <input type="submit" name="btn-new-pass" class="btn user__cta" id="btn-login" value="CẬP NHẬT" />
                         <?php echo form_error('account'); ?>
                     </form>
+
                 </div>
                 <div class="sidebar__user">
                     <div class="avatar__user">
