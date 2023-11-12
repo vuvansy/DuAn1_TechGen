ce18c55992b166d79bf5fe8ea8ce8e7ce514a62c
1stephen37 <ngamingyahoo@gmail.com>
2023-11-12 12:21:47 +0700

    features/admin-order_d fix table center and fix select have many option

--- admin/modules/order/views/successView.php
+++ admin/modules/order/views/successView.php
@@ -91,7 +91,252 @@ get_header();
                                 </td>
 
                                 <td class="detail">
-                                    <a href="">
+                                    <a href="?mod=order&action=detail">
+                                        Chi tiết
+                                    </a>
+                                </td>
+
+                            </tr>
+                            <tr class="list-item">
+
+                                <td class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <path d="M14.6667 2H5.33333C4.59333 2 4 2.59333 4 3.33333V12.6667C4 13.0203 4.14048 13.3594 4.39052 13.6095C4.64057 13.8595 4.97971 14 5.33333 14H14.6667C15.0203 14 15.3594 13.8595 15.6095 13.6095C15.8595 13.3594 16 13.0203 16 12.6667V3.33333C16 2.59333 15.4 2 14.6667 2ZM14.6667 3.33333V12.6667H5.33333V3.33333H14.6667Z" fill="black"/>
+                                    </svg>
+                                </td>
+
+                                <td class="id">
+                                    IT96281
+                                </td>
+
+                                <td class="date">
+                                    03/11/2023
+                                </td>
+
+                                <td class="quantity">
+                                    2
+                                </td>
+
+                                <td class="total">
+                                    44.000.000đ
+                                </td>
+
+                                <td class="status">
+                                    giao thành công
+                                </td>
+
+                                <td class="detail">
+                                    <a href="?mod=order&action=detail">
+                                        Chi tiết
+                                    </a>
+                                </td>
+
+                            </tr>
+                            <tr class="list-item">
+
+                                <td class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <path d="M14.6667 2H5.33333C4.59333 2 4 2.59333 4 3.33333V12.6667C4 13.0203 4.14048 13.3594 4.39052 13.6095C4.64057 13.8595 4.97971 14 5.33333 14H14.6667C15.0203 14 15.3594 13.8595 15.6095 13.6095C15.8595 13.3594 16 13.0203 16 12.6667V3.33333C16 2.59333 15.4 2 14.6667 2ZM14.6667 3.33333V12.6667H5.33333V3.33333H14.6667Z" fill="black"/>
+                                    </svg>
+                                </td>
+
+                                <td class="id">
+                                    IT96281
+                                </td>
+
+                                <td class="date">
+                                    03/11/2023
+                                </td>
+
+                                <td class="quantity">
+                                    2
+                                </td>
+
+                                <td class="total">
+                                    44.000.000đ
+                                </td>
+
+                                <td class="status">
+                                    giao thành công
+                                </td>
+
+                                <td class="detail">
+                                    <a href="?mod=order&action=detail">
+                                        Chi tiết
+                                    </a>
+                                </td>
+
+                            </tr>
+                            <tr class="list-item">
+
+                                <td class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <path d="M14.6667 2H5.33333C4.59333 2 4 2.59333 4 3.33333V12.6667C4 13.0203 4.14048 13.3594 4.39052 13.6095C4.64057 13.8595 4.97971 14 5.33333 14H14.6667C15.0203 14 15.3594 13.8595 15.6095 13.6095C15.8595 13.3594 16 13.0203 16 12.6667V3.33333C16 2.59333 15.4 2 14.6667 2ZM14.6667 3.33333V12.6667H5.33333V3.33333H14.6667Z" fill="black"/>
+                                    </svg>
+                                </td>
+
+                                <td class="id">
+                                    IT96281
+                                </td>
+
+                                <td class="date">
+                                    03/11/2023
+                                </td>
+
+                                <td class="quantity">
+                                    2
+                                </td>
+
+                                <td class="total">
+                                    44.000.000đ
+                                </td>
+
+                                <td class="status">
+                                    giao thành công
+                                </td>
+
+                                <td class="detail">
+                                    <a href="?mod=order&action=detail">
+                                        Chi tiết
+                                    </a>
+                                </td>
+
+                            </tr>
+                            <tr class="list-item">
+
+                                <td class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <path d="M14.6667 2H5.33333C4.59333 2 4 2.59333 4 3.33333V12.6667C4 13.0203 4.14048 13.3594 4.39052 13.6095C4.64057 13.8595 4.97971 14 5.33333 14H14.6667C15.0203 14 15.3594 13.8595 15.6095 13.6095C15.8595 13.3594 16 13.0203 16 12.6667V3.33333C16 2.59333 15.4 2 14.6667 2ZM14.6667 3.33333V12.6667H5.33333V3.33333H14.6667Z" fill="black"/>
+                                    </svg>
+                                </td>
+
+                                <td class="id">
+                                    IT96281
+                                </td>
+
+                                <td class="date">
+                                    03/11/2023
+                                </td>
+
+                                <td class="quantity">
+                                    2
+                                </td>
+
+                                <td class="total">
+                                    44.000.000đ
+                                </td>
+
+                                <td class="status">
+                                    giao thành công
+                                </td>
+
+                                <td class="detail">
+                                    <a href="?mod=order&action=detail">
+                                        Chi tiết
+                                    </a>
+                                </td>
+
+                            </tr>
+                            <tr class="list-item">
+
+                                <td class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <path d="M14.6667 2H5.33333C4.59333 2 4 2.59333 4 3.33333V12.6667C4 13.0203 4.14048 13.3594 4.39052 13.6095C4.64057 13.8595 4.97971 14 5.33333 14H14.6667C15.0203 14 15.3594 13.8595 15.6095 13.6095C15.8595 13.3594 16 13.0203 16 12.6667V3.33333C16 2.59333 15.4 2 14.6667 2ZM14.6667 3.33333V12.6667H5.33333V3.33333H14.6667Z" fill="black"/>
+                                    </svg>
+                                </td>
+
+                                <td class="id">
+                                    IT96281
+                                </td>
+
+                                <td class="date">
+                                    03/11/2023
+                                </td>
+
+                                <td class="quantity">
+                                    2
+                                </td>
+
+                                <td class="total">
+                                    44.000.000đ
+                                </td>
+
+                                <td class="status">
+                                    giao thành công
+                                </td>
+
+                                <td class="detail">
+                                    <a href="?mod=order&action=detail">
+                                        Chi tiết
+                                    </a>
+                                </td>
+
+                            </tr>
+                            <tr class="list-item">
+
+                                <td class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <path d="M14.6667 2H5.33333C4.59333 2 4 2.59333 4 3.33333V12.6667C4 13.0203 4.14048 13.3594 4.39052 13.6095C4.64057 13.8595 4.97971 14 5.33333 14H14.6667C15.0203 14 15.3594 13.8595 15.6095 13.6095C15.8595 13.3594 16 13.0203 16 12.6667V3.33333C16 2.59333 15.4 2 14.6667 2ZM14.6667 3.33333V12.6667H5.33333V3.33333H14.6667Z" fill="black"/>
+                                    </svg>
+                                </td>
+
+                                <td class="id">
+                                    IT96281
+                                </td>
+
+                                <td class="date">
+                                    03/11/2023
+                                </td>
+
+                                <td class="quantity">
+                                    2
+                                </td>
+
+                                <td class="total">
+                                    44.000.000đ
+                                </td>
+
+                                <td class="status">
+                                    giao thành công
+                                </td>
+
+                                <td class="detail">
+                                    <a href="?mod=order&action=detail">
+                                        Chi tiết
+                                    </a>
+                                </td>
+
+                            </tr>
+                            <tr class="list-item">
+
+                                <td class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <path d="M14.6667 2H5.33333C4.59333 2 4 2.59333 4 3.33333V12.6667C4 13.0203 4.14048 13.3594 4.39052 13.6095C4.64057 13.8595 4.97971 14 5.33333 14H14.6667C15.0203 14 15.3594 13.8595 15.6095 13.6095C15.8595 13.3594 16 13.0203 16 12.6667V3.33333C16 2.59333 15.4 2 14.6667 2ZM14.6667 3.33333V12.6667H5.33333V3.33333H14.6667Z" fill="black"/>
+                                    </svg>
+                                </td>
+
+                                <td class="id">
+                                    IT96281
+                                </td>
+
+                                <td class="date">
+                                    03/11/2023
+                                </td>
+
+                                <td class="quantity">
+                                    2
+                                </td>
+
+                                <td class="total">
+                                    44.000.000đ
+                                </td>
+
+                                <td class="status">
+                                    giao thành công
+                                </td>
+
+                                <td class="detail">
+                                    <a href="?mod=order&action=detail">
                                         Chi tiết
                                     </a>
                                 </td>
