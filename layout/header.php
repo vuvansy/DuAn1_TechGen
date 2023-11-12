c63c9908006ef648551ef811559470df822b95f7
vuvansy <vuvansy3001@gmail.com>
2023-11-12 21:32:15 +0700

    TN fix bug

--- layout/header.php
+++ layout/header.php
@@ -33,7 +33,7 @@
     <!-- Embed Font  -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <!-- Import file Style CSS -->
     <link href="public/style.css" rel="stylesheet" type="text/css" />
 
