54761747c9f04f7c518aa140ea9d2f4374325464
NnKha <nhatkha2018@gmail.com>
2023-11-13 17:54:58 +0700

    features/admin-product_nk fix-with + form

--- admin/modules/product/views/addView.php
+++ admin/modules/product/views/addView.php
@@ -14,82 +14,85 @@ get_header();
                 <a href="?mod=product&action=list" class="product-btn-list">Danh sách</a>
                 <a href="?mod=product&action=add" class="product-btn-add">Thêm mới</a>
             </div>
-            <div class="container-product">
-                <div class="head">
-                    <div class="inline">
-                        <div class="icon">
-                            <img src="./public/images/icon/border-all.svg" alt="">
-                        </div>
-                        <div class="heading">
-                            Danh sách hàng hóa
-                        </div>
-                    </div>
-                </div>
-                <div class="container-form">
-                    <div class="container-form-input">
-                        <div class="input-text">
-                            <p>Mã sản phẩm</p>
-                            <input type="text" placeholder="ps...">
-                        </div>
-                        <div class="input-text">
-                            <p>Tên sản phẩm</p>
-                            <input type="text" placeholder="Tên sản phẩm">
-                        </div>
-                        <div class="input-text">
-                            <p>Đơn giá</p>
-                            <input type="text" placeholder="Đơn giá">
-                        </div>
-                        <div class="input-text">
-                            <p>Giảm giá</p>
-                            <input type="text" placeholder="Giảm giá">
-                        </div>
-                        <div class="input-text">
-                            <p>Số lượng</p>
-                            <input type="text" placeholder="Số lượng">
-                        </div>
-                        <div class="input-text">
-                            <p>Loại hàng</p>
-                            <select class="select-box" name="" id="">
-                                <option value="">Điện Thoại</option>
-                                <option value="">Laptop</option>
-                            </select>
-                        </div>
-                        <div class="input-text">
-                            <p>Ngày nhập</p>
-                            <input type="date" placeholder="Ngày nhập">
-                        </div>
-                        <div class="input-text">
-                            <p>Mã sản phẩm</p>
-                            <div class="option">
-                                <div class="option-input">
-                                    <input class="input-checkbox" type="checkbox" id="block">
-                                    <p class="text-checkbox">Hiển thị sản phẩm</p>
-                                </div>
-                                <div class="option-input">
-                                    <input class="input-checkbox" type="checkbox" id="none">
-                                    <p class="text-checkbox">Sản phẩm đặc biệt</p>
-                                </div>
+            <div class="box-container-product">
+                <div class="container-product">
+                    <div class="head">
+                        <div class="inline">
+                            <div class="icon">
+                                <img src="./public/images/icon/border-all.svg" alt="">
+                            </div>
+                            <div class="heading">
+                                Danh sách hàng hóa
                             </div>
                         </div>
-                        <div class="input-text">
-                            <p>Hình ảnh</p>
-                            <input class="input-file" type="file">
-                        </div>
-                        <div class="input-text">
-                            <p>Ảnh chi tiết (4)</p>
-                            <input class="input-file" type="file">
-                        </div>
-                        <div class="input-desc">
-                            <p>Mô tả ngắn</p>
-                            <textarea>Mô tả ngắn</textarea>
-                        </div>
-                        <div class="input-desc">
-                            <p>Nội dung</p>
-                            <textarea class="input-content">Nội dung</textarea>
-                        </div>
-                        <div class="box-btn-update">
-                        <a href="?mod=product&action=list" class="btn-update"><p>Thêm mới</p></a>
-                        </div>
+                    </div>
+                    <div class="container-form">
+                        <form action="" id="add-product" class="container-form-input" enctype="multipart/form-data" method="POST">
+                            <div class="input-text">
+                                 <label for="">Mã sản phẩm</label>
+                                <input type="text" id="id_product" name="id_product" placeholder="ps...">
+                            </div>
+                            <div class="input-text">
+                                 <label for="">Tên sản phẩm</label>
+                                <input type="text" id="product_name" name="product_name" placeholder="Tên sản phẩm">
+                            </div>
+                            <div class="input-text">
+                               <label for="">Đơn giá</label>
+                                <input type="text" id="product_price" name="product_price" placeholder="Đơn giá">
+                            </div>
+                            <div class="input-text">
+                                <label for="">Giảm giá</label>
+                                <input type="text" id="product_sale" name="product_sale" placeholder="Giảm giá">
+                            </div>
+                            <div class="input-text">
+                                 <label for="">Số lượng</label>
+                                <input type="text" id="product_quantity" name="product_quantity" placeholder="Số lượng">
+                            </div>
+                            <div class="input-text">
+                                <label for="">Loại hàng</label>
+                                <select class="select-box" name="" id="">
+                                    <option value="">Điện Thoại</option>
+                                    <option value="">Laptop</option>
+                                </select>
+                            </div>
+                            <div class="input-text">
+                                <label for="">Ngày nhập</label>
+                                <input type="date" id="date_import" name="date_import" placeholder="Ngày nhập">
+                            </div>
+                            <div class="input-text">
+                                <label for="">Tùy chọn</label>
+                                <div class="option">
+                                    <div class="option-input">
+                                        <input class="input-checkbox" type="checkbox" id="product_status" name="product_status" >
+                                        <p class="text-checkbox">Hiển thị sản phẩm</p>
+                                    </div>
+                                    <div class="option-input">
+                                        <input class="input-checkbox" type="checkbox" id="special" name="special" >
+                                        <p class="text-checkbox">Sản phẩm đặc biệt</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="input-text">
+                               <label for="">Hình ảnh</label>
+                                <input class="input-file" name="product_image" id="product_image" type="file">
+                            </div>
+                            <div class="input-text">
+                                <label for="">Ảnh chi tiết (4)</label>
+                                <input class="input-file" id="delivery_name" name="delivery_name" type="file">
+                            </div>
+                            
+                            <div class="input-desc">
+                                 <label for="">Mô tả ngắn</label>
+                                <textarea name="product_desc" id="product_desc" >Mô tả ngắn</textarea>
+                            </div>
+                            <div class="input-desc">
+                                 <label for="">Nội dung</label>
+                                <textarea name="product_content" id="product_content" class="input-content">Nội dung</textarea>
+                            </div>
+                            <div class="box-btn-button">
+                            <input class="btn-button" type="submit" name="btn-add-product" id="btn-list" value="Thêm mới" >
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -97,14 +100,8 @@ get_header();
         </div>
     </div>
     </div>
-   
+
 </main>
-<script>
-       let check = document.getElementById("block");
-       check.checked = true ; 
-       let uncheck = document.getElementById("none");
-       uncheck.checked = false ; 
-    </script>
 <?php
 get_footer();
 ?>
