2dd3875e5e48a2e92ec193566a3fac6d5c03927e
mtienha <tiennmpd08296@fpt.edu.vn>
2023-11-13 08:30:55 +0700

    features/page_t fix bug

--- modules/page/views/contactView.php
+++ modules/page/views/contactView.php
@@ -1,11 +1,9 @@
-
 <?php
-get_header();
-//show_array($list_users);
+get_header()
 ?>
-
-<main>
-<section id="contact">
+    
+    <main>
+      <section id="contact">
         <div class="container">
           <h2 class="contact__heading">THÔNG TIN LIÊN HỆ</h2>
           <p class="contact__desc">
@@ -60,11 +58,64 @@ get_header();
           </ul>
         </div>
       </section>
-</main>
-
-
+    <footer>
+      <div class="container">
+        <div class="footer__top">
+          <div class="logo">
+            <a href="">
+              <img src="public/images/logo.png" alt="TechGen" class="logo" />
+            </a>
+          </div>
+          <div class="column">
+            <h3 class="title">HỖ TRỢ - DỊCH VỤ</h3>
+            <ul class="list">
+              <li>Mua hàng tra góp</li>
+              <li>Hướng dẫn cài đặt và thanh toán</li>
+              <li>Tra cứu đơn hàng</li>
+              <li>Chăm sóc khách hàng</li>
+            </ul>
+          </div>
+          <div class="column">
+            <h3 class="title">THÔNG TIN LIÊN HỆ</h3>
+            <ul class="list">
+              <li>Cao Đẳng FPT Polytechnic</li>
+              <li>Lô 32 đề thám quận 12</li>
+              <li>Website: techgen.id.vn</li>
+              <li>Email:techgen.gmail.com</li>
+            </ul>
+          </div>
+          <div class="column">
+            <h3 class="title">CHÍNH SÁCH</h3>
+            <ul class="list">
+              <li>Chính sách sử dụng website</li>
+              <li>Thanh toán khi nhận hàng</li>
+              <li>Tra cứu đơn hàng</li>
+              <li>Chính sách bảo hành</li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer__bottom">
+          <div class="icon">
+            <a href="#">
+              <i class="fa-brands fa-twitter"></i>
+            </a>
+            <a href="#">
+              <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a href="#">
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i class="fa-brands fa-pinterest"></i>
+            </a>
+          </div>
+          <span class="copyright"
+            >©2021 - 2024 Công Ty Cổ Phần Thương Mại - Dịch Vụ TechGen Copyright
+            All 2023 By TechGen</span>
+        </div>
+      </div>
+    </footer>
+  </main>
 <?php
-get_footer();
-?>  
-    
-
+get_footer()
+?>
