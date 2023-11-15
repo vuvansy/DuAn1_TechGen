c682670650e08639a71abd0a9e863e7ad910fa56
vuvansy <vuvansy3001@gmail.com>
2023-11-16 03:30:19 +0700

    features/BEU-user_s TN Code BE Register

--- modules/users/models/indexModel.php
+++ modules/users/models/indexModel.php
@@ -1,1 +1,17 @@
 <?php
+// Hàm Kiểm tra dữ liệu trùng
+function user_exists($username, $email)
+{
+    $check_user = db_num_rows("SELECT * FROM `user` WHERE `email` = '$email' OR `username` = '{$username}'");
+    if ($check_user > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Thêm User vào Database
+function add_user($data)
+{
+    return db_insert('user', $data);
+}
