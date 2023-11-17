a3a82d458865e31b46b0cfb3a0856d47c563509a
vuvansy <vuvansy3001@gmail.com>
2023-11-17 22:06:30 +0700

    Fix Sản phẩm liên quan

--- helper/data.php
+++ helper/data.php
@@ -18,11 +18,26 @@ function  get_category()
         return $item;
 }
 
+//Truy vấn một LOẠI HÀNG theo mã loại
+function get_category_by_id($id)
+{
+    $item = db_fetch_row("SELECT * FROM `category` WHERE `id_category` = {$id}");
+    if (!empty($item))
+        return $item;
+}
+
 
 // Lấy tất cả SẢN PHẨM
 function  get_product()
 {
     $item = db_fetch_array("SELECT *FROM `product`");
+    if (!empty($item))
+        return $item;
+}
+
+function  get_product_new()
+{
+    $item = db_fetch_array("SELECT *FROM `product` ORDER BY `id_product` DESC  LIMIT 0, 5 ");
     if (!empty($item))
         return $item;
 }
