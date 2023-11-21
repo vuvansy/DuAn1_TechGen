a46b74e39166915db2084f19ea00dab05193449a
1stephen37 <ngamingyahoo@gmail.com>
2023-11-21 12:22:03 +0700

    features/BEA-order_d fix các lỗi nhỏ

--- admin/modules/order/views/indexView.php
+++ admin/modules/order/views/indexView.php
@@ -7,6 +7,8 @@ $status = [
     1 => 'đang vận chuyển',
     2 => 'đã hủy',
     3 => 'đã giao hàng',
+    4 => 'đã hủy',
+    5 => 'đã giao hàng',
 ];
 $oder_wait_html = '';
 foreach ($orderListWait as $value) {
