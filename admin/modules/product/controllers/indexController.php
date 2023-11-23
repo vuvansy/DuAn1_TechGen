70d033c5d095fde37b440c7852e023f6a451f5e2
NnKha <nhatkha2018@gmail.com>
2023-11-23 20:02:35 +0700

    features/BEA-product_nk

--- admin/modules/product/controllers/indexController.php
+++ admin/modules/product/controllers/indexController.php
@@ -11,7 +11,7 @@ function indexAction()
     /*load_*/
     if (isset($_POST['search-product-btn'])) {
         $search_product = $_POST['box-search-product'];
-        echo $search_product;
+        // echo $search_product;
         $get_product = search_product_by_name($search_product);
     } else {
         $get_product = get_product();
@@ -27,7 +27,6 @@ function addAction()
     global $error;
     $data = [];
     if (isset($_POST['btn-add-product'])) {
-
         $error = array();
 
         //tên sp
@@ -134,48 +133,53 @@ function editAction()
         $id_product = $_GET['id'];
         $product_by_id = (get_product_by_id($id_product))[0];
         // show_array($product_by_id);
-        $data["product_by_id"] = $product_by_id;
-        // show_array($data["product_by_id"]);
-        load_view('edit', $data);
+        // $data["product_by_id"] = $product_by_id;
+        // // show_array($data["product_by_id"]);
+        // load_view('edit', $data);
     }
    
+    global $error;
 
     if (isset($_POST['btn-edit-product'])) {
-
-        global $error;
+        $error = array();
         $data = [];
 
          //tên sp
          if (empty($_POST['product_name'])) {
+            // $_SESSION['product_name'] = '<span class="massew">Vui lòng nhập đủ thông tin !</span>';
             $error['product_name'] = "Không được để trống tên sản phẩm";
             // echo "lỗi";
         } else {
-            $data['product_name'] = $_POST['product_name'];
+            $data_new['product_name'] = $_POST['product_name'];
         }
          //giá sp
          if (empty($_POST['product_price'])) {
+            // $_SESSION['product_price'] = '<span class="massew">Vui lòng nhập đủ thông tin !</span>';
             $error['product_price'] = "Không được để trống giá sản phẩm";
             // echo "lỗi";
         } else {
-            $data['product_price'] = $_POST['product_price'];
+            $data_new['product_price'] = $_POST['product_price'];
         }
 
         //số lượng sp
         if (empty($_POST['product_quantity'])) {
+            // $_SESSION['product_quantity'] = '<span class="massew">Vui lòng nhập đủ thông tin !</span>';
             $error['product_quantity'] = "Không được để trống số lượng";
             // echo "lỗi";
         } else {
-            $data['product_quantity'] = $_POST['product_quantity'];
+            $data_new['product_quantity'] = $_POST['product_quantity'];
         }
 
         //ngày nhập sp
         if (empty($_POST['date_import'])) {
+        
+            // $_SESSION['date_import'] = '<span class="massew">Vui lòng nhập đủ thông tin !</span>';
             $error['date_import'] = "Không được để trống ngày nhập";
             // echo "lỗi";
         } else {
-            $data['date_import'] = $_POST['date_import'];
+            $data_new['date_import'] = $_POST['date_import'];
         }
-
+        //ảnh sp
         if (($_FILES['product_image_new']['name'] != '')) {
             $data_new['product_image'] = $_FILES['product_image_new']['name']; 
             $fileTmpPath = $_FILES['product_image_new']['tmp_name'];
@@ -187,20 +191,22 @@ function editAction()
 
          //mô tả ngắn sp
          if (empty($_POST['product_desc'])) {
+            // $_SESSION['product_desc'] = '<span class="massew">Vui lòng nhập đủ thông tin !</span>';
             $error['product_desc'] = "Không được để trống mô tả ngắn";
             // echo "lỗi";
         } else {
-            $data['product_desc'] = $_POST['product_desc'];
+            $data_new['product_desc'] = $_POST['product_desc'];
         }
 
         //nội dung sp
         if (empty($_POST['product_content'])) {
+            // $_SESSION['product_content'] = '<span class="massew">Vui lòng nhập đủ thông tin !</span>';
             $error['product_content'] = "Không được để trống nội dung";
             // echo "lỗi";
         } else {
-            $data['product_content'] = $_POST['product_content'];
+            $data_new['product_content'] = $_POST['product_content'];
         }
-
+        
         $data_new['id_product'] = $_POST['id_product'];
         $data_new['product_sale'] = $_POST['product_sale'];             
         $data_new['id_category'] = $_POST['id_category']; 
@@ -219,12 +225,31 @@ function editAction()
 
         if (empty($error)) {
             db_update('product', $data_new, $where);
+            // update_img
+            // $gallery_name = [];
+            // if ($_FILES['gallery_name']['name'] != '') {
+            //     for ($i = 0; $i < count($_FILES['gallery_name']['name']); $i++) {
+            //         $gallery['id_gallery'] = null;
+            //         $gallery['gallery_name'] = $_FILES['gallery_name']['name'][$i];
+            //         $fileTmpPath = $_FILES['gallery_name']['tmp_name'][$i];
+            //         $destination = '../public/images/gallery/' . $gallery['gallery_name'];
+            //         move_uploaded_file($fileTmpPath, $destination);
+            //         db_update('gallery', $gallery,$id_product);
+            //     }
+            // }
             header('location: ?mod=product&action=index');
             $_SESSION['masew'] = '<span class="massew">Cập nhật thành công !</span>';
+           
+            
         } else {
+            // header('location: ?mod=product&action=edit&id=' . $id_product);
+            // load_view('edit', $data);
+            // show_array($error); 
             $_SESSION['masew'] = '<span class="massew">Vui lòng nhập đủ thông tin !</span>';
         }
     }
+    $data["product_by_id"] = $product_by_id;
+    load_view('edit', $data);
    
 }
 
