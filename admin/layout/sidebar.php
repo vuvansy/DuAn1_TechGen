3e84a0d9d693605beca7a44e5d2a53d112e7d600
vuvansy <vuvansy3001@gmail.com>
2023-11-29 16:27:49 +0700

    fix bug URL end Thống kê

--- admin/layout/sidebar.php
+++ admin/layout/sidebar.php
@@ -3,22 +3,22 @@
     <h1 class="heading lv1">DANH MỤC</h1>
     <ul class="sidebar-nav__list">
         <li class="sidebar-nav__item sidebar-nav__active">
-            <a href="?mod=category&action=index" class="sidebar-nav__link">Loai hàng</a>
+            <a href="?mod=category&action=index" class="sidebar-nav__link"><i class="fa-solid fa-table-list"></i> Loai hàng</a>
         </li>
         <li class="sidebar-nav__item">
-            <a href="?mod=product&action=index" class="sidebar-nav__link">Hàng hóa</a>
+            <a href="?mod=product&action=index" class="sidebar-nav__link"><i class="fa-solid fa-table-list"></i> Hàng hóa</a>
         </li>
         <li class="sidebar-nav__item">
-            <a href="?mod=users&action=index" class="sidebar-nav__link">Khách hàng</a>
+            <a href="?mod=users&action=index" class="sidebar-nav__link"><i class="fa-solid fa-user"></i> Khách hàng</a>
         </li>
         <li class="sidebar-nav__item">
-            <a href="?mod=comment&action=index" class="sidebar-nav__link">Bình luận</a>
+            <a href="?mod=comment&action=index" class="sidebar-nav__link"><i class="fa-solid fa-message"></i> Bình luận</a>
         </li>
         <li class="sidebar-nav__item">
-            <a href="?mod=order&action=index" class="sidebar-nav__link">Đơn hàng</a>
+            <a href="?mod=order&action=index" class="sidebar-nav__link"><i class="fa-solid fa-cart-shopping"></i> Đơn hàng</a>
         </li>
         <li class="sidebar-nav__item">
-            <a href="?mod=statistical&action=index" class="sidebar-nav__link">Thống kê</a>
+            <a href="?mod=statistical&action=index" class="sidebar-nav__link"><i class="fa-solid fa-chart-line"></i> Thống kê</a>
         </li>
     </ul>
 </div>
--- modules/order/views/indexView.php
+++ modules/order/views/indexView.php
@@ -228,13 +228,13 @@ if (isset($_SESSION['is_login'])) {
                             }
                         ?>
                             <div class="product__item">
-                                <a href="?mod=product&cation=index&id=<?php echo $item['id_product'] ?>">
+                                <a href="?mod=product&action=index&id=<?php echo $item['id_product'] ?>">
                                     <img src="<?php echo $imagesURL ?>/product/<?php echo $item['product_image'] ?>" alt="<?php echo $category['category_name'] ?>" class="thumb">
                                 </a>
                                 <div class="product__info">
                                     <h3 class="product__info--title">
                                         <span class="product-brand"><?php echo $category['category_name'] ?></span>
-                                        <a href="?mod=product&cation=index&id=<?php echo $item['id_product'] ?>" class="line-clamp break-all line-2">
+                                        <a href="?mod=product&action=index&id=<?php echo $item['id_product'] ?>" class="line-clamp break-all line-2">
                                             <?php echo $item['product_name'] ?>
                                         </a>
                                     </h3>
@@ -273,8 +273,8 @@ if (isset($_SESSION['is_login'])) {
 <?= $noti ?>
 
 <?php
-    if (isset($_SESSION['is_login'])) {
-        echo '
+if (isset($_SESSION['is_login'])) {
+    echo '
             <script>
                 (function validate() {
         const name_delivery = document.getElementById("name_delivery");
@@ -394,7 +394,7 @@ if (isset($_SESSION['is_login'])) {
     })();
             </script>
         ';
-    }
+}
 ?>
 
 
